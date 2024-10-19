1fc1741ac93b1d73b19afb9bb373958ada1c3cfe
MatthiasBart <m.bartholomaeus02@gmail.com>
2024-10-19 13:31:37 +0200

    init commit

---
+++ PtPPrototype/PtPPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PtPPrototypeApp.swift
+//  PtPPrototype
+//
+//  Created by Matthias Bartholomaeus on 19.10.24.
+//
+
+import SwiftUI
+
+@main
+struct PtPPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PtPPrototypeTests/PtPPrototypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PtPPrototypeTests.swift
+//  PtPPrototypeTests
+//
+//  Created by Matthias Bartholomaeus on 19.10.24.
+//
+
+import Testing
+@testable import PtPPrototype
+
+struct PtPPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
